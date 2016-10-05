922fc67229c187841167109617edba9313279295
dreiven <albertoarranzv@gmail.com>
2016-10-05 21:43:32 +0200

    se crea metodo de apertura de archivos

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,4 +1,5 @@
 package Principal;
+
 import java.io.File;
 
 import javax.swing.Icon;
@@ -26,133 +27,131 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Principal extends JFrame  {
+public class Principal extends JFrame {
 
-	
-	 JTree explorador;
-	 DefaultMutableTreeNode Raiz;
-	 DefaultTreeCellRenderer render;
-	 String ruta2="";
-	 private JButton Abrir;
-	 private JButton Descargar;
-	  
-	 public Principal(){
-     
-	 setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\workspace\\Explorer\\Imagenes\\folder.png"));
-      this.setTitle("EXPLORADOR");
-	  this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-	  Raiz=new DefaultMutableTreeNode("Raiz");
-	  explorador=new JTree(Raiz);
-	  
-	  explorador.addTreeWillExpandListener(new TreeWillExpandListener() {
-	  	public void treeWillCollapse(TreeExpansionEvent event) {
-	  		render=(DefaultTreeCellRenderer)explorador.getCellRenderer();
-	  		 Icon folder2 = Utils.escalar_Imagen("/Imagen/carpeta2.png",10);
-		  	  render.setClosedIcon(folder2);
-	  	}
-	  	public void treeWillExpand(TreeExpansionEvent event) {
-	  		render=(DefaultTreeCellRenderer)explorador.getCellRenderer();
-	  	  Icon folderopn = Utils.escalar_Imagen("/Imagen/carpeta.png",10);
-	  	  render.setOpenIcon(folderopn);
-	  	 
-	  	}
-	  });
-	
-	  DefaultTreeCellRenderer render=(DefaultTreeCellRenderer)explorador.getCellRenderer();
-	
-	 // Icon folderclsd =  Obtener_Imagenes.escalar_Imagen("/Imagen/folder.png",10);
-	  //render.setLeafIcon(folderclsd);
-	  Icon folderopn = Utils.escalar_Imagen("/Imagen/carpeta.png",10);
-	  render.setOpenIcon(folderopn);
-	  Icon folder2 = Utils.escalar_Imagen("/Imagen/carpeta2.png",10);
-	  render.setClosedIcon(folder2);
-	
-	  explorador.addTreeSelectionListener(new TreeSelectionListener(){
+	JTree explorador;
+	DefaultMutableTreeNode Raiz;
+	DefaultTreeCellRenderer render;
+	String ruta2 = "";
+	private JButton Abrir;
+	private JButton Descargar;
 
+	public Principal() {
 
-	   public void valueChanged(TreeSelectionEvent e) {
-	    try{
-	    DefaultMutableTreeNode sel=(DefaultMutableTreeNode)explorador.getLastSelectedPathComponent();
-	    agregarHijos(sel);
-	    for(int i=0;i<sel.getChildCount();i++){
-	     DefaultMutableTreeNode nieto=(DefaultMutableTreeNode)sel.getChildAt(i);
-	     
-	     
-	     
-	     agregarHijos(nieto);
-	    
-	    }
-	    }catch(NullPointerException npe){
-	     
-	    }
-	   }
-	   
-	  });
-	  
-	  for(int i=0;i<File.listRoots().length;i++){
-	   DefaultMutableTreeNode hijo=new DefaultMutableTreeNode(File.listRoots()[i]);
-	   Raiz.add(hijo);
-	  }  
-	  getContentPane().setLayout(null);
-	  JScrollPane scrollPane = new JScrollPane(explorador);
-	  scrollPane.setBounds(0, 0, 442, 329);
-	  getContentPane().add(scrollPane);
-	  
-	  Abrir = new JButton("Abrir");
-	  Abrir.addActionListener(new ActionListener() {
-	  public void actionPerformed(ActionEvent e) {
-		  for(int i=0;i<File.listRoots().length;i++){
-			  
-			   
-			   Utils.openarch(ruta2);
-			  }  
-	  		
-	  		
-	  		
-	  	}
-	  });
-	  Abrir.setBounds(35, 367, 91, 23);
-	  getContentPane().add(Abrir);
-	  
-	  Descargar = new JButton("Descargar");
-	  Descargar.setBounds(216, 367, 91, 23);
-	  getContentPane().add(Descargar);
-	 }
-	 
-	 public void agregarHijos(DefaultMutableTreeNode padre) throws NullPointerException{
-	  if(padre!=Raiz){
-	   File fpadre=obtenerRuta(padre);
-	   if(fpadre.isDirectory()){
-	    for(int i=0;i<fpadre.list().length;i++){
-	     DefaultMutableTreeNode hijo=new DefaultMutableTreeNode(fpadre.list()[i]);
-	     padre.add(hijo);
-	     File fhijo=obtenerRuta(hijo);
-	     ruta2 = fhijo.getPath();
-	     
-	    }
-	   }
-	  }
-	 }
+		setIconImage(Toolkit.getDefaultToolkit().getImage(
+				"C:\\workspace\\Explorer\\Imagenes\\folder.png"));
+		this.setTitle("EXPLORADOR");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		Raiz = new DefaultMutableTreeNode("Raiz");
+		explorador = new JTree(Raiz);
 
-//	 public String Ruta(String p){
-//	  String ruta="";
-////	  for(int i=0;i<p.getPath().length-1;i++){
-////	   ruta=ruta+p.getPath()[i+1]+"\\";
-////	  }
-//	  
-//	  return ruta;
-//	 }
-//	 
-	 
-	 
-	 
-	 
-	 public File obtenerRuta(DefaultMutableTreeNode p){
-	  String ruta="";
-	  for(int i=0;i<p.getPath().length-1;i++){
-	   ruta=ruta+p.getPath()[i+1]+"\\";
-	  }
-	  File f=new File(ruta);
-	  return f;
-	 }
+		explorador.addTreeWillExpandListener(new TreeWillExpandListener() {
+			public void treeWillCollapse(TreeExpansionEvent event) {
+				render = (DefaultTreeCellRenderer) explorador.getCellRenderer();
+				Icon folder2 = Utils.escalar_Imagen("/Imagen/carpeta2.png", 10);
+				render.setClosedIcon(folder2);
+			}
+
+			public void treeWillExpand(TreeExpansionEvent event) {
+				render = (DefaultTreeCellRenderer) explorador.getCellRenderer();
+				Icon folderopn = Utils
+						.escalar_Imagen("/Imagen/carpeta.png", 10);
+				render.setOpenIcon(folderopn);
+
+			}
+		});
+
+		DefaultTreeCellRenderer render = (DefaultTreeCellRenderer) explorador
+				.getCellRenderer();
+
+		// Icon folderclsd =
+		// Obtener_Imagenes.escalar_Imagen("/Imagen/folder.png",10);
+		// render.setLeafIcon(folderclsd);
+		Icon folderopn = Utils.escalar_Imagen("/Imagen/carpeta.png", 10);
+		render.setOpenIcon(folderopn);
+		Icon folder2 = Utils.escalar_Imagen("/Imagen/carpeta2.png", 10);
+		render.setClosedIcon(folder2);
+
+		explorador.addTreeSelectionListener(new TreeSelectionListener() {
+
+			public void valueChanged(TreeSelectionEvent e) {
+				try {
+					DefaultMutableTreeNode sel = (DefaultMutableTreeNode) explorador
+							.getLastSelectedPathComponent();
+					agregarHijos(sel);
+					File fhijo = obtenerRuta(sel);
+					ruta2 = fhijo.getPath();
+					for (int i = 0; i < sel.getChildCount(); i++) {
+						DefaultMutableTreeNode nieto = (DefaultMutableTreeNode) sel
+								.getChildAt(i);
+
+						agregarHijos(nieto);
+
+					}
+				} catch (NullPointerException npe) {
+
+				}
+			}
+
+		});
+
+		for (int i = 0; i < File.listRoots().length; i++) {
+			DefaultMutableTreeNode hijo = new DefaultMutableTreeNode(
+					File.listRoots()[i]);
+			Raiz.add(hijo);
+		}
+		getContentPane().setLayout(null);
+		JScrollPane scrollPane = new JScrollPane(explorador);
+		scrollPane.setBounds(0, 0, 442, 329);
+		getContentPane().add(scrollPane);
+
+		Abrir = new JButton("Abrir");
+		Abrir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				Utils.openarch(ruta2);
+
+			}
+		});
+		Abrir.setBounds(35, 367, 91, 23);
+		getContentPane().add(Abrir);
+
+		Descargar = new JButton("Descargar");
+		Descargar.setBounds(216, 367, 91, 23);
+		getContentPane().add(Descargar);
+	}
+
+	public void agregarHijos(DefaultMutableTreeNode padre)
+			throws NullPointerException {
+		if (padre != Raiz) {
+			File fpadre = obtenerRuta(padre);
+			if (fpadre.isDirectory()) {
+				for (int i = 0; i < fpadre.list().length; i++) {
+					DefaultMutableTreeNode hijo = new DefaultMutableTreeNode(
+							fpadre.list()[i]);
+					padre.add(hijo);
+
+				}
+			}
+		}
+	}
+
+	// public String Ruta(String p){
+	// String ruta="";
+	// // for(int i=0;i<p.getPath().length-1;i++){
+	// // ruta=ruta+p.getPath()[i+1]+"\\";
+	// // }
+	//
+	// return ruta;
+	// }
+	//
+
+	public File obtenerRuta(DefaultMutableTreeNode p) {
+		String ruta = "";
+		for (int i = 0; i < p.getPath().length - 1; i++) {
+			ruta = ruta + p.getPath()[i + 1] + "\\";
+		}
+		File f = new File(ruta);
+		return f;
+	}
 }
